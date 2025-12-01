308a079271773c7113e7a09a08ea28645de13c16
shahedzahaykeh <szahayke@macalester.edu>
2025-12-01 13:45:30 -0600

    Update UIKiltGraphics.java

--- UI/UIKiltGraphics.java
+++ UI/UIKiltGraphics.java
@@ -1,1 +1,12 @@
+import edu.macalester.graphics.*;
+import edu.macalester.graphics.events.MouseButton;
+
+public class UIKiltGraphics {
+
+    private CanvasWindow canvas;
+    private Move humanChoice;
+    private boolean choiceMade;
+    
+
+
 
